1e69e12bc29f6b944bd5ea5d258a13a85537a0cf
liuweiwei <505831001@qq.com>
2019-12-05 14:56:47 +0800

    v1.0

--- src/com/liuweiwei/bean/Employee.java
+++ src/com/liuweiwei/bean/Employee.java
@@ -2,40 +2,39 @@ package com.liuweiwei.bean;
 
 import javax.validation.constraints.Pattern;
 
-import org.hibernate.validator.constraints.Email;
-
 public class Employee {
     private Integer empId;
 
-    @Pattern(regexp="(^[a-zA-Z0-9_-]{6,16}$)|(^[\u2E80-\u9FFF]{2,5})",message="用户名必须是2-5位中文或者6-16位英文和数字的组合")
+    @Pattern(regexp = "(^[a-zA-Z0-9_-]{6,16}$)|(^[\u2E80-\u9FFF]{2,5})", message = "用户名必须是2-5位中文或者6-16位英文和数字的组合")
     private String empName;
     private String gender;
-    // @Email
-    @Pattern(regexp="^([a-z0-9_\\.-]+)@([\\da-z\\.-]+)\\.([a-z\\.]{2,6})$", message="邮箱格式不正确")
+
+    @Pattern(regexp = "^([a-z0-9_\\.-]+)@([\\da-z\\.-]+)\\.([a-z\\.]{2,6})$", message = "邮箱格式不正确")
     private String email;
     private Integer dId;
     private Department department;
 
-	public Employee() {
-		super();
-	}
-	public Employee(Integer empId, String empName, String gender, String email,
-			Integer dId) {
-		super();
-		this.empId = empId;
-		this.empName = empName;
-		this.gender = gender;
-		this.email = email;
-		this.dId = dId;
-	}
+    public Employee() {
+        super();
+    }
 
-	public Department getDepartment() {
-		return department;
-	}
-	public void setDepartment(Department department) {
-		this.department = department;
-	}
-	public Integer getEmpId() {
+    public Employee(Integer empId, String empName, String gender, String email,
+                    Integer dId) {
+        super();
+        this.empId = empId;
+        this.empName = empName;
+        this.gender = gender;
+        this.email = email;
+        this.dId = dId;
+    }
+
+    public Department getDepartment() {
+        return department;
+    }
+    public void setDepartment(Department department) {
+        this.department = department;
+    }
+    public Integer getEmpId() {
         return empId;
     }
     public void setEmpId(Integer empId) {
@@ -68,8 +67,13 @@ public class Employee {
 
     @Override
     public String toString() {
-        return "Employee [empId=" + empId + ", empName=" + empName
-                + ", gender=" + gender + ", email=" + email + ", dId=" + dId
-                + "]";
+        return "Employee{" +
+                "empId=" + empId +
+                ", empName='" + empName + '\'' +
+                ", gender='" + gender + '\'' +
+                ", email='" + email + '\'' +
+                ", dId=" + dId +
+                ", department=" + department +
+                '}';
     }
 }
